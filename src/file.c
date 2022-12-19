5932f191fac1dacfd69958006ff4f6956329095f
Rafael Kitover <rkitover@gmail.com>
2022-12-19 16:14:05 +0000

    Use fputc(), single-arg putc() unsupported by MSVC
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- src/file.c
+++ src/file.c
@@ -605,7 +605,7 @@ fname_print(const char *inname)
 	for (i = 0; i < n; i++) {
 		unsigned char c = CAST(unsigned char, inname[i]);
 		if (isprint(c)) {
-			putc(c);
+			fputc(c, stdout);
 			continue;
 		}
 		file_octal(c);
